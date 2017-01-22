9ef2cb504bc279c9c84f4b3c808b28c00e2b6dcd
Rob Hitt <robhitt@gmail.com>
2017-01-22 11:28:07 -0500

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,19 @@
-# Implement your procedural solution here!
+def largest_palindrome_product
+  max = 0
+  (100..999).each do |first_num|
+    (100..999).each do |second_num|
+      current_total = first_num * second_num
+      if is_pallindrome?(current_total) && current_total > max
+        max = current_total
+      end
+    end
+  end
+  return max
+end
+
+def is_pallindrome?(num)
+  num = num.to_s
+  num == num.reverse
+end
+
+# puts largest_palindrome_product
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,23 @@
 # Implement your object-oriented solution here!
+class LargestPalindromeProduct
+  def initialize
+    @max = 0
+  end
+
+  def answer
+    (100..999).each do |first_num|
+      (100..999).each do |second_num|
+        current_total = first_num * second_num
+        if is_pallindrome?(current_total) && current_total > @max
+          @max = current_total
+        end
+      end
+    end
+    return @max
+  end
+
+  def is_pallindrome?(num)
+    num = num.to_s
+    num == num.reverse
+  end
+end
